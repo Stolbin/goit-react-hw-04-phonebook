27586423f6d498676010a862dc0578c304bfde06
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-03-03 11:34:58 +0200

    upd

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -49,7 +49,9 @@ const Phonebook = ({ onSubmit }) => {
           type="tel"
           name="number"
           value={number}
+          id="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          maxlength="20"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={handleChange}

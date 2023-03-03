588aae7a965c4cdee978e88f268b6ce3912634d7
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-03-03 13:07:08 +0200

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -40,6 +40,7 @@ const Phonebook = ({ onSubmit }) => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          autoComplete="off"
           onChange={handleChange}
         />
       </Label>
@@ -49,11 +50,11 @@ const Phonebook = ({ onSubmit }) => {
           type="tel"
           name="number"
           value={number}
-          id="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           maxLength={20}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          autoComplete="off"
           onChange={handleChange}
         />
       </Label>
